459fd459515395831162c587a7e200a39b171632
vYORKv <vYORKv@gmail.com>
2022-03-10 11:52:11 -0600

    Cleaned up movement code
    Ship facing mouse works (with turn speed), and movement now has proper acceleration and friction. Now need to make movement based on mouse location with current code.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,13 +1,13 @@
 extends KinematicBody2D
 
-const acceleration = 20
-const max_speed = 300
-const friction = 60
-const speed = 200
+const acceleration = 5
+const max_speed = 5
+const friction = 4
+const speed = 10
 const turn_speed = 0.2
 
-var velocity = Vector2()
-#var velocity = Vector2.ZERO
+#var velocity = Vector2()
+var velocity = Vector2.ZERO
 onready var TweenNode = get_node("Tween")
 
 var inputs = {
@@ -45,42 +45,24 @@ func _ready():
 
 func _physics_process(delta):
 	var mouse_position = get_global_mouse_position()
-	velocity = Vector2()
+	var input_vector = Vector2.ZERO
+	var direction = (mouse_position - self.position).normalized()
 	# initial and final x-vector of basis
 	var initial_transform_x = self.transform.x
 	var final_transform_x = (mouse_position - self.global_position).normalized()
 	# interpolate
 	TweenNode.interpolate_method(self, '_set_rotation', initial_transform_x, final_transform_x, turn_speed, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	TweenNode.start()
-	if Input.is_action_pressed("ui_up"):
-		velocity += Vector2(speed, 0).rotated(rotation)
-	elif Input.is_action_pressed("ui_down"):
-		velocity = Vector2(-speed * .7, 0).rotated(rotation)
-	elif Input.is_action_pressed("ui_left"):
-		velocity = Vector2(0, -speed * .7).rotated(rotation)
-	elif Input.is_action_pressed("ui_right"):
-		velocity = Vector2(0, speed * .7).rotated(rotation)
+	look_at(mouse_position)
+	
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = (input_vector - mouse_position).normalized()
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
-#	GetInput()
-	velocity = move_and_slide(velocity)
-	
-	
-#	var input_vector = Vector2.ZERO
-#	var mouse_position = get_global_mouse_position()
-#	look_at(mouse_position)
-#
-#	var direction = (mouse_position - self.position).normalized()
-#	print(direction)
-#	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-#	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-#	input_vector = input_vector.normalized()
-#	if input_vector != Vector2.ZERO:
-#		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
-#	else:
-#		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
-	
-#	find_dir()
+	move_and_collide(velocity)
 
 func _set_rotation(new_transform):
 	# apply tweened x-vector of basis
