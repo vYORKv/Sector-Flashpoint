922e298b42056db7af58155a6b70e5da8407ab11
vYORKv <vYORKv@gmail.com>
2022-03-10 15:19:56 -0600

    Finished directional movement
    Found strafing motion solution using tangent() method. Basic locomotion is now complete. Next is combat/patrol speed and bullets.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,52 +1,23 @@
 extends KinematicBody2D
 
 const acceleration = 5
-const max_speed = 5
+const max_speed = 3
 const friction = 4
 const speed = 10
 const turn_speed = 0.2
 
-#var velocity = Vector2()
 var velocity = Vector2.ZERO
-onready var TweenNode = get_node("Tween")
 
-var inputs = {
-	"ui_right": Vector2.RIGHT,
-	"ui_left": Vector2.LEFT,
-	"ui_up": Vector2.UP,
-	"ui_down": Vector2.DOWN
-}
+onready var TweenNode = get_node("Tween")
 
 func _ready():
 	pass
 
-#func GetInput():
-#	var mouse_position = get_global_mouse_position()
-#	velocity = Vector2()
-#	# initial and final x-vector of basis
-#	var initial_transform_x = self.transform.x
-#	var final_transform_x = (mouse_position - self.global_position).normalized()
-#	# interpolate
-#	TweenNode.interpolate_method(self, '_set_rotation', initial_transform_x, final_transform_x, turn_speed, Tween.TRANS_LINEAR, Tween.EASE_OUT)
-#	TweenNode.start()
-#
-#	if Input.is_action_pressed("ui_up"):
-#		speed = lerp(speed, max_speed, get_process_delta_time()*acceleration)
-#		velocity = Vector2(speed, 0).rotated(rotation)
-#	elif Input.is_action_pressed("ui_down"):
-#		speed = lerp(speed, max_speed, get_process_delta_time()*acceleration)
-#		velocity = Vector2(-speed * .7, 0).rotated(rotation)
-#	elif Input.is_action_pressed("ui_left"):
-#		speed = lerp(speed, max_speed, get_process_delta_time()*acceleration)
-#		velocity = Vector2(0, -speed * .7).rotated(rotation)
-#	elif Input.is_action_pressed("ui_right"):
-#		speed = lerp(speed, max_speed, get_process_delta_time()*acceleration)
-#		velocity = Vector2(0, speed * .7).rotated(rotation)
-
 func _physics_process(delta):
 	var mouse_position = get_global_mouse_position()
-	var input_vector = Vector2.ZERO
 	var direction = (mouse_position - self.position).normalized()
+	var direction_x = direction.tangent()
+	
 	# initial and final x-vector of basis
 	var initial_transform_x = self.transform.x
 	var final_transform_x = (mouse_position - self.global_position).normalized()
@@ -55,11 +26,14 @@ func _physics_process(delta):
 	TweenNode.start()
 	look_at(mouse_position)
 	
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector = (input_vector - mouse_position).normalized()
-	if input_vector != Vector2.ZERO:
-		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
+	if Input.get_action_strength("ui_up"):
+		velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
+	elif Input.get_action_strength("ui_down"):
+		velocity = velocity.move_toward(-direction * max_speed/2, acceleration * delta)
+	elif Input.get_action_strength("ui_left"):
+		velocity = velocity.move_toward(direction_x * max_speed/2, acceleration * delta)
+	elif Input.get_action_strength("ui_right"):
+		velocity = velocity.move_toward(-direction_x * max_speed/2, acceleration * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 	move_and_collide(velocity)
