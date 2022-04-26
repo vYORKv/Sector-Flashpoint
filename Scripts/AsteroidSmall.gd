369d9ee35cdea315d96f39c067cf2f2cf530950e
york-iv <104084580+york-iv@users.noreply.github.com>
2022-04-26 14:29:06 -0500

    Created small asteroid scene with bullet hit
    +Asteroid scene. +Randomly generate asteroid sprite on instance. +Added collision for ships and hurtbox. +Allowed bullet to hit.

---
+++ Scripts/AsteroidSmall.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+const SM_ASTER_01 = preload("res://Graphics/Asteroids/small_asteroid_01.png")
+const SM_ASTER_02 = preload("res://Graphics/Asteroids/small_asteroid_02.png")
+const SM_ASTER_03 = preload("res://Graphics/Asteroids/small_asteroid_03.png")
+
+var sprites = [SM_ASTER_01, SM_ASTER_02, SM_ASTER_03]
+
+onready var sprite = get_node("Sprite") 
+
+func _ready():
+	randomize()
+	
+	sprite.set_texture(get_sprite())
+
+func get_sprite():
+	var random_sprite = sprites[randi() % sprites.size()]
+	return random_sprite
+
+func hit(bullet):
+	pass
--- Scripts/GameLaunch.gd
+++ Scripts/GameLaunch.gd
@@ -1,7 +1,21 @@
 extends Node
 
 const DUMMY = preload("res://Scenes/Actors/Dummy.tscn")
+const AST_SM = preload("res://Scenes/Objects/AsteroidSmall.tscn")
+
+onready var AS1 = get_node("AsteroidSpawn1")
+onready var AS2 = get_node("AsteroidSpawn2")
+onready var AS3 = get_node("AsteroidSpawn3")
 
 func _ready():
 	var dummy = DUMMY.instance()
 	add_child(dummy)
+	var a1 = AST_SM.instance()
+	var a2 = AST_SM.instance()
+	var a3 = AST_SM.instance()
+	add_child(a1)
+	add_child(a2)
+	add_child(a3)
+	a1.position = AS1.global_position
+	a2.position = AS2.global_position
+	a3.position = AS3.global_position
